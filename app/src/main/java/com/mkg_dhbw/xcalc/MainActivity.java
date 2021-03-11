db42ccc50238252ad9dba48646fe29455446fc18
Antonio Markic <antonio.markic@web.de>
2021-03-11 09:38:15 +0100

    Changes in UI

--- app/src/main/java/com/mkg_dhbw/xcalc/MainActivity.java
+++ app/src/main/java/com/mkg_dhbw/xcalc/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mkg_dhbw.xcalc;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -20,11 +21,12 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_chart, R.id.navigation_home, R.id.navigation_history)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
 }
--- app/src/main/java/com/mkg_dhbw/xcalc/ui/chart/ChartFragment.java
+++ app/src/main/java/com/mkg_dhbw/xcalc/ui/chart/ChartFragment.java
@@ -1,4 +1,4 @@
-package com.mkg_dhbw.xcalc.ui.dashboard;
+package com.mkg_dhbw.xcalc.ui.chart;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.mkg_dhbw.xcalc.R;
 
-public class DashboardFragment extends Fragment {
+public class ChartFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private ChartViewModel chartViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        chartViewModel =
+                new ViewModelProvider(this).get(ChartViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_chart, container, false);
+        final TextView textView = root.findViewById(R.id.text_chart);
+        chartViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
